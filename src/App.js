64b2a8f12bd465153344e6c2394db370e1af1108
kamruz-zaman8991 <70217992+kamruz-zaman8991@users.noreply.github.com>
2021-12-16 00:36:44 +0600

    sign up form designed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './Component/SIgn In Form/SignIn';
+import SignUp from './Component/SignUpForm/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignIn></SignIn>
+      <SignUp></SignUp>
     </div>
   );
 }

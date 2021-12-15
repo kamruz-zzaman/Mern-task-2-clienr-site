5460c10875c1bca8a8afe7db60be6666d24e153b
kamruz-zaman8991 <70217992+kamruz-zaman8991@users.noreply.github.com>
2021-12-16 01:38:30 +0600

    nav added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Component/Home/Home';
 import SignIn from './Component/SignInForm/SignIn';
 import SignUp from './Component/SignUpForm/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <SignIn></SignIn>
-      <SignUp></SignUp>
+      <BrowserRouter>
+        <Home></Home>
+        <Routes>
+          <Route path="signin" element={<SignIn />}></Route>
+        </Routes>
+        <Routes>
+          <Route path="signUp" element={<SignUp />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
